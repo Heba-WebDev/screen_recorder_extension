c1ac4e4106a38efab6c907fe87b81cfe1318854a
Heba-Webdev23 <jtvonline@yahoo.com>
2023-10-02 16:04:54 +0300

    Changed to the popup

--- js/popup.js
+++ js/popup.js
@@ -1,43 +1,42 @@
 const closeWindow = document.querySelector(".close_btn");
+const startRecordingBtn = document.querySelector("#start_video");
+const stopRecordingBtn = document.querySelector("#stop_video");
 
+// Closes the extension window
 closeWindow.addEventListener("click", () => {
   window.close();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  // we page is fully loaded
-  const startRecordingBtn = document.querySelector("#start_video");
-  const stopRecordingBtn = document.querySelector("#stop_video");
-
-  startRecordingBtn.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { action: "request_recording" },
-        function (response) {
-          if (!chrome.runtime.lastError) {
-            console.log(response);
-          } else {
-            console.log(chrome.runtime.lastError);
-          }
+// Starts recording
+startRecordingBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "request_recording" },
+      function (response) {
+        if (!chrome.runtime.lastError) {
+          console.log(response);
+        } else {
+          console.log(chrome.runtime.lastError);
         }
-      );
-    });
+      }
+    );
   });
+});
 
-  stopRecordingBtn.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { action: "stop_video" },
-        function (response) {
-          if (!chrome.runtime.lastError) {
-            console.log(response);
-          } else {
-            console.log(chrome.runtime.lastError);
-          }
+// Stop recording
+stopRecordingBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "stop_video" },
+      function (response) {
+        if (!chrome.runtime.lastError) {
+          console.log(response);
+        } else {
+          console.log(chrome.runtime.lastError);
         }
-      );
-    });
+      }
+    );
   });
 });
